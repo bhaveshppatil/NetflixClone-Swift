910e910855308ca7ac4e0aca30e0a34db8c86c99
bhaveshppatil <bhavesh.patil0325@gmail.com>
2022-02-22 12:52:26 +0530

    Updated UITableViewCell

--- Controllers/HomeViewController.swift
+++ Controllers/HomeViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let homeFeedTable : UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UIViewTableCell.self, forCellReuseIdentifier: UIViewTableCell.identifire)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
+        
+        view.addSubview(homeFeedTable)
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0,width: view.bounds.width, height: 450))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
-    */
+}
 
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)  -> UITableViewCell{
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: UIViewTableCell.identifire, for: indexPath) as? UIViewTableCell else {
+                return UITableViewCell()
+            }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
 }
